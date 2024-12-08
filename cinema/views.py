77da474d8da200e9c3c82be2e35898a67571c413
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-08 15:03:11 +0200

    write default serializers and define methods in movieviewset

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,43 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
+from cinema.serializers import CinemaHallSerializer, GenreSerializer, ActorSerializer, MovieSerializer, \
+    MovieSessionSerializer, MovieRetrieveSerializer, MovieListSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return MovieRetrieveSerializer
+        elif self.action == "list":
+            return MovieListSerializer
+        return self.serializer_class
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            return queryset.prefetch_related("genres", "actors")
+        return queryset
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
